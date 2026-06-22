ec9b42869b662fc24af6bbc39d6147db05cfeff3
Your Name <you@example.com>
2026-06-22 12:25:06 +0200

    Prepare package compatibility and license

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "MattermostSwift",
     platforms: [
-        .iOS(.v26),
-        .macOS(.v26),
+        .iOS(.v18),
+        .macOS(.v15),
     ],
     products: [
         .library(
